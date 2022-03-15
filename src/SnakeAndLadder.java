edb2f1bdbbe6fefdc15d731bb49abea94593cf54
vigneshj04 <vigneshjmax@gmail.com>
2022-03-15 09:56:56 +0530

    Updated: Ladder,Snake & No-play chances

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -9,11 +9,11 @@ public class SnakeAndLadder {
         switch (dieRolls) {
             case SNAKE:
                 System.out.println("Die Rolls ...... " + dieRolls);
-                System.out.println("You got bite by a Snake & Your Position Goes to "+ (int) Math.floor(Math.random() * 10) % 16 +1);
+                System.out.println("You got bite by a Snake & Your Position Goes to " + (int) Math.floor(Math.random() * 10) % 16 + 1);
                 break;
             case LADDER:
                 System.out.println("Die Rolls ...... " + dieRolls);
-                System.out.println("Got a Ladder & Your Position Goes to " + Position + (int) Math.floor(Math.random() * 10) % 24+ 1);
+                System.out.println("Got a Ladder & Your Position Goes to " + Position + (int) Math.floor(Math.random() * 10) % 24 + 1);
                 break;
             default:
                 System.out.println("Die Rolls ......" + Position);
