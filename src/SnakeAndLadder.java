4b7efb1e5472d13279af2448141c2abd319c0eec
vigneshj04 <vigneshjmax@gmail.com>
2022-03-15 08:13:03 +0530

    Added: Snake and ladder Game

---
+++ src/SnakeAndLadder.java
@@ -0,0 +1,7 @@
+public class SnakeAndLadder {
+    public static void main(String[] args) {
+        System.out.println("Hello,Welcome");
+        int Position =0;
+        System.out.println("Player Position " + Position);
+    }
+}
