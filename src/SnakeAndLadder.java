ed7f1d8776de335c8bca4b77fd87374449a36239
vigneshj04 <vigneshjmax@gmail.com>
2022-03-15 09:17:36 +0530

    Updated: Die Rolls First time

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -1,7 +1,13 @@
+
 public class SnakeAndLadder {
     public static void main(String[] args) {
-        System.out.println("Hello,Welcome");
-        int Position =0;
-        System.out.println("Player Position " + Position);
+        int Player;
+        int Position = 0;
+        int dieRolls = (int) Math.floor((Math.random() * 10) % 6 + 1);
+        System.out.println("Player position is 0");
+        System.out.println("Die is Rolling..... " + dieRolls);
+        int newPosition = Position + dieRolls;
+        System.out.println("Player Position " + newPosition);
     }
+
 }
