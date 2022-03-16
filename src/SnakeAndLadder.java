23643ff4a311d997376338e7b811c27726a019f1
vigneshj04 <vigneshjmax@gmail.com>
2022-03-16 07:59:00 +0530

    Updated: Till Player reaches 100

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -1,27 +1,36 @@
 
 public class SnakeAndLadder {
+
     public static void main(String[] args) {
         int Player;
-        int Position = 0;
+        int playerPosition = 0;
         final int LADDER = 2;
         final int SNAKE = 1;
-        int dieRolls = (int) Math.floor((Math.random() * 10) % 6 + 1);
-        int option = (int) Math.floor((Math.random() * 10) % 3);
-        System.out.println("Option "+ option);
-        switch (option) {
-            case SNAKE:
-                System.out.println("Die Rolls ...... " + dieRolls);
-                Position = Position - dieRolls;
-                System.out.println("You got bite by a Snake & Your Position Goes to " + Position );
-                break;
-            case LADDER:
-                System.out.println("Die Rolls ...... " + dieRolls);
-                Position = Position + dieRolls;
-                System.out.println("Got a Ladder & Your Position Goes to " + Position );
-                break;
-            default:
-                //System.out.println("Die Rolls ......" + dieRolls);
-                System.out.println("Player position is Same");
+        while (playerPosition <= 100) {
+            int dieRolls = (int) Math.floor((Math.random() * 10) % 6 + 1);
+            int option = (int) Math.floor((Math.random() * 10) % 3);
+            switch (option) {
+                case SNAKE:
+                    System.out.println("Die Rolls ...... " + dieRolls);
+                    System.out.println("Option is ...... " + option);
+                    playerPosition = playerPosition - dieRolls;
+                    System.out.println("You got a bite by Snake & Your Position Goes to " + playerPosition);
+                    if (playerPosition == 0)  {
+                        System.out.println("Player position is Same " +playerPosition);
+                    }
+                    break;
+                case LADDER:
+                    System.out.println("Die Rolls ...... " + dieRolls);
+                    System.out.println("Option is ...... " + option);
+                    playerPosition = playerPosition + dieRolls;
+                    System.out.println("Got a Ladder & Your Position Goes to " + playerPosition);
+                    break;
+                default:
+            }
+            playerPosition++;
         }
     }
 }
+
+
+
