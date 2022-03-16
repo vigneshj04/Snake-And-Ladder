f190614d46b461c673ccf502245091fcf6e4774d
vigneshj04 <vigneshjmax@gmail.com>
2022-03-16 08:04:23 +0530

    Updated: Game starts With Player Position 0

--- src/SnakeAndLadder.java
+++ src/SnakeAndLadder.java
@@ -3,11 +3,8 @@ public class SnakeAndLadder {
     public static void main(String[] args) {
         int Player;
         int Position = 0;
-        int dieRolls = (int) Math.floor((Math.random() * 10) % 6 + 1);
+        System.out.println("Let's start a game..... ");
         System.out.println("Player position is 0");
-        System.out.println("Die is Rolling..... " + dieRolls);
-        int newPosition = Position + dieRolls;
-        System.out.println("Player Position " + newPosition);
     }
 
 }
